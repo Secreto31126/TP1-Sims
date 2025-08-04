455d87f985f6f5ad4ed5294fb799f8fe46dc505d
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-08-04 15:03:34 -0300

    Use Particle pointers for better memory use

--- types/particle.go
+++ types/particle.go
@@ -3,14 +3,14 @@ package types
 import "math"
 
 type Particle struct {
-	Id        int        // Identificador de la particula
-	X, Y      float64    // Posicion de la particula
-	Radius    float64    // Radio de la particula
-	Property  float64    // Propiedad de la particula
-	Neighbors []Particle // Vecinos de la particula
+	Id        int         // Identificador de la particula
+	X, Y      float64     // Posicion de la particula
+	Radius    float64     // Radio de la particula
+	Property  float64     // Propiedad de la particula
+	Neighbors []*Particle // Vecinos de la particula
 }
 
-func (p Particle) BorderDistanceTo(other Particle) float64 {
+func (p Particle) BorderDistanceTo(other *Particle) float64 {
 	dx := p.X - other.X
 	dy := p.Y - other.Y
 	return math.Sqrt(dx*dx+dy*dy) - (p.Radius + other.Radius)
