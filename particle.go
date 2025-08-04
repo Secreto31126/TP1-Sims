1260a5db8513ec281bbc95d8ccd509639ced6634
Tomás Raiti <46955459+Secreto31126@users.noreply.github.com>
2025-08-04 13:25:33 -0300

    Add radius

--- particle.go
+++ particle.go
@@ -3,6 +3,7 @@ package main
 type Particle struct {
 	id        int        // Identificador de la particula
 	X, Y      float64    // Posicion de la particula
+	radius    float64    // Radio de la particula
 	property  int        // Propiedad de la particula
 	neighbors []Particle // Vecinos de la particula
 }
